4f408914d725f074d01899f6e01d48856cc644ae
zhangyongfa <zhangyongfa@xindecolink.com>
2023-09-07 18:25:49 +0800

    chore:新增less支持

---
+++ src/pages/login/login.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+import style from "./login.module.less";
+import IMG from "../../assets/images/cli.png";
+const LoginPage: FC = () => {
+    return <div className={`${style.login}` }>
+        <span>一个🌰</span>
+        <img src={IMG}/>
+    </div> 
+   
+}
+
+export default LoginPage;
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,32 @@
-import { resolve } from 'path';
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const LoaderFactory = require('./bundle/loaders');
 
 module.exports = {
   entry: {
-    main: resolve('src/index.tsx')
+    main: path.resolve(__dirname, './src/index.tsx')
   },
   output: {
-    path: resolve(process.cwd(), 'dist')
+    filename: "static/js/[name].js", // 每个输出js的名称
+    clean: true,
+    path: path.resolve(__dirname, './dist'),
   },
   module: {
-    rules: [
-      {
-        test: /\.(ts|tsx)$/,
-        use: {
-          loader: 'swc-loader'
-        }
-      }
-    ]
-  }
+    rules: [...new LoaderFactory().getLoaders()]
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".jsx", ".js"],
+  },
+  // plugins
+  plugins: [
+    new HtmlWebpackPlugin({
+      // 复制 'index.html' 文件，并自动引入打包输出的所有资源（js/css）
+      template: path.join(__dirname, "/public/index.html"),
+      // 压缩html资源
+      minify: {
+        collapseWhitespace: true, //去空格
+        removeComments: true, // 去注释
+      },
+    }),
+  ],
 }
