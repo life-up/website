cc30756086c67df13053a3732200542eab466e98
zhangyongfa <zhangyongfa@xindecolink.com>
2023-09-22 18:34:47 +0800

    chore: udpate hardhat

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,15 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig } from 'hardhat/config';
+import '@nomicfoundation/hardhat-toolbox';
+require('hardhat-plugin-solidity');
+import '@typechain/hardhat';
+import '@nomicfoundation/hardhat-ethers';
+import '@nomicfoundation/hardhat-chai-matchers';
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
+  solidity: '0.8.19',
+  paths: {
+    tests: './test/contracts',
+  },
 };
 
 export default config;
